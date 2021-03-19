20a8e444c01b02c8bb50c7c1c41dff576de531b3
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-19 09:54:20 +0000

    feat: compose feed loader publisher

--- Snap/Application/SceneDelegate.swift
+++ Snap/Application/SceneDelegate.swift
@@ -7,19 +7,40 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    private var APP_ID: String { "605448f84977a585e87b67f8" }
+    
+    private lazy var httpClient: HTTPClient = {
+        return URLSessionHTTPClient(session: .init(configuration: .ephemeral))
+    }()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = FeedUIComposer.compose()
+            window.rootViewController = FeedUIComposer.compose(loader: makeFeedLoader)
             self.window = window
             window.makeKeyAndVisible()
         }
+    }
+}
+
+private extension SceneDelegate {
+        
+    // MARK:- Loaders
+    func makeFeedLoader() -> AnyPublisher<[Post], Error> {
+        var request = URLRequest(endpoint: .feed())
+        request.addValue(APP_ID, forHTTPHeaderField: "app-id")
+        return httpClient
+            .dispatchPublisher(for: request)
+            .tryMap(FeedMapper.map)
+            .dispatchOnMainQueue()
+            .eraseToAnyPublisher()
     }
 }
 
