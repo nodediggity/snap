80be595b263503763dd80bac6379e25e4cb51982
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-19 09:51:43 +0000

    feat: layout initial feed items

---
+++ SnapTests/Modules/Feed/UI/Views/FeedList/FeedSnapshotTests.swift
@@ -0,0 +1,42 @@
+//
+//  FeedSnapshotTests.swift
+//  SnapTests
+//
+//  Created by Gordon Smith on 19/03/2021.
+//
+
+import Foundation
+import XCTest
+import SwiftUI
+
+@testable import Snap
+
+class FeedSnapshotTests: XCTestCase {
+
+    func test_feed_with_content() {
+        let content = feedWithContent
+        let viewModel = FeedViewModel(loader: { $0(.success(content)) })
+        let sut = makeSUT(viewModel: viewModel)
+        
+        viewModel.load()
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
+    }
+}
+
+private extension FeedSnapshotTests {
+    func makeSUT(viewModel: FeedViewModel) -> UIViewController {
+        let rootView = FeedListView(viewModel: viewModel)
+        let controller = UIHostingController(rootView: rootView)
+        controller.loadViewIfNeeded()
+        return controller
+    }
+    
+    
+    var feedWithContent: [Post] {
+        return [
+            Post(id: "id 1", imageURL: makeURL(), likeCount: 25, user: .init(id: UUID().uuidString, name: "Some Name", imageURL: makeURL())),
+            Post(id: "id 2", imageURL: makeURL(), likeCount: 34, user: .init(id: UUID().uuidString, name: "Some'Other Name", imageURL: makeURL()))
+        ]
+    }
+}
