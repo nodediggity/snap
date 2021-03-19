cad02f7ff6c97cabc60a2759a9b61ff8b9ad4f1e
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-19 06:34:48 +0000

    feat: compose `FeedUI` with new `FeedUIComposer`

---
+++ Snap/Modules/Feed/UI/Views/FeedListView.swift
@@ -0,0 +1,15 @@
+//
+//  FeedListView.swift
+//  Snap
+//
+//  Created by Gordon Smith on 19/03/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct FeedListView: View {
+    var body: some View {
+        Text("Feed.")
+    }
+}
