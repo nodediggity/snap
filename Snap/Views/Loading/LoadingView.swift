a8ddb5ea44e08d87ae52c5011c7d582a71181765
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-19 10:09:28 +0000

    feat: create a `LoadingView`

---
+++ Snap/Views/Loading/LoadingView.swift
@@ -0,0 +1,33 @@
+//
+//  LoadingView.swift
+//  Snap
+//
+//  Created by Gordon Smith on 19/03/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct LoadingView: View {
+
+    @State private var scale: CGFloat = 0
+    @State private var opacity: Double = 0
+
+    var body: some View {
+        let animation = Animation
+            .easeIn(duration: 1.25)
+            .repeatForever(autoreverses: false)
+
+        return Circle()
+            .scaleEffect(scale)
+            .opacity(opacity)
+            .onAppear {
+                scale = 0
+                opacity = 1
+                withAnimation(animation) {
+                    scale = 1
+                    opacity = 0
+                }
+            }
+    }
+}
