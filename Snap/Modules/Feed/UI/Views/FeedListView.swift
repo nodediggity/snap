58a1f7873765e31490cecef768acdf98baa278e4
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-20 18:10:28 +0000

    refactor: use `APP_ID_KEY` constant

--- Snap/Modules/Feed/UI/Views/FeedListView.swift
+++ Snap/Modules/Feed/UI/Views/FeedListView.swift
@@ -10,41 +10,41 @@ import SwiftUI
 
 struct FeedListView: View {
     
-    @ObservedObject private(set) var viewModel: FeedViewModel
+    @EnvironmentObject var imageProvider: ImageLoaderProvider
+    @ObservedObject private var viewModel: FeedViewModel
+    
+    init(viewModel: FeedViewModel) {
+        self.viewModel = viewModel
+    }
     
     var body: some View {
-        renderViewFor(viewModel.state)
-    }
-}
-
-private extension FeedListView {
-    func renderViewFor(_ state: ViewState<[Post]>) -> some View  {
-        return Group {
-            
-            if case .loading = state {
-                LoadingView()
-                    .frame(width: 50.0, height: 50.0)
-                    .foregroundColor(Color("3dc6a7"))
-                    .onAppear(perform: viewModel.loadFeed)
-            }
-            
-            if case let .loaded(feed) = state {
-                ScrollView(.vertical, showsIndicators: true) {
-                    LazyVStack(spacing: 0) {
-                        ForEach(feed, id: \.id) { item in
-                            
-                            AsyncImageView()
-                                .frame(height: 400)
-                                .shimmer()
-                            
-                            
-                            
-                            
+        
+        Group {
+            switch viewModel.state {
+                case .loading:
+                    LoadingView()
+                        .frame(width: 50.0, height: 50.0)
+                        .foregroundColor(Color("3dc6a7"))
+                        .onAppear(perform: viewModel.loadFeed)
+                    
+                case let .loaded(feed):
+                    ScrollView(.vertical, showsIndicators: true) {
+                        LazyVStack(spacing: 0) {
+                            ForEach(feed, id: \.id) { item in
+                                
+                                AsyncImageView(
+                                    viewModel: .init(
+                                        imageURL: item.imageURL,
+                                        loadImagePublisher: imageProvider.make(),
+                                        imageTransformer: UIImage.init
+                                    )
+                                ).frame(height: 400)
+                                
+                            }
                         }
                     }
-                }
+                default: EmptyView()
             }
-            
         }
     }
 }
