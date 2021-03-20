47f44b63457d35daa3c03b02ea0ea01d52369357
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-20 18:06:05 +0000

    feat: create `AsyncImageView`

--- Snap/Modules/Common/UI/Views/AsnycImageView/AsyncImageView.swift
+++ Snap/Modules/Common/UI/Views/AsnycImageView/AsyncImageView.swift
@@ -5,13 +5,40 @@
 //  Created by Gordon Smith on 20/03/2021.
 //
 
-import Foundation
+import UIKit
 import SwiftUI
 
 struct AsyncImageView: View {
     
-    var body: some View {
-        Color(.tertiaryLabel)
+    typealias ViewModel = AsyncImageViewModel<UIImage>
+    
+    @ObservedObject private var viewModel: ViewModel
+    
+    private var canRetry: Bool
+    
+    init(viewModel: ViewModel, canRetry: Bool = true) {
+        self.viewModel = viewModel
+        self.canRetry = canRetry
     }
     
+    var body: some View {
+        Group {
+            switch viewModel.state {
+                case .loading:
+                    Color(.tertiarySystemFill)
+                        // .shimmer() - This modifier seems to be causing a 40 - 50 mb spike in memory usage...
+                        .onAppear(perform: viewModel.loadImage)
+                case let .loaded(image):
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                case .error where canRetry:
+                    Text("oops.")
+                default:
+                    EmptyView()
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .onDisappear(perform: viewModel.cancel)
+    }
 }
