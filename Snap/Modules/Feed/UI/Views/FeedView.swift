4f70925df0dd711995570d341e22f31a18219262
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-21 07:25:47 +0000

    refactor: extract feed / list behaviour to common `ListView`

--- Snap/Modules/Feed/UI/Views/FeedView.swift
+++ Snap/Modules/Feed/UI/Views/FeedView.swift
@@ -1,5 +1,5 @@
 //
-//  FeedListView.swift
+//  FeedView.swift
 //  Snap
 //
 //  Created by Gordon Smith on 19/03/2021.
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct FeedListView: View {
+struct FeedView: View {
     
     @ObservedObject private var viewModel: FeedViewModel
     
@@ -17,24 +17,21 @@ struct FeedListView: View {
     }
     
     var body: some View {
-        
         Group {
             switch viewModel.state {
                 case .loading:
+                    
                     LoadingView()
                         .frame(width: 50.0, height: 50.0)
                         .foregroundColor(Color("3dc6a7"))
                         .onAppear(perform: viewModel.loadFeed)
                     
                 case let .loaded(feed):
-                    ScrollView(.vertical, showsIndicators: true) {
-                        VStack(spacing: 0) {
-                            ForEach(feed, id: \.id) { item in
-                                FeedCard(item: item)
-                            }
-                        }
-                        .background(Color(.systemBackground))
+                    
+                    ListView(items: feed) { item in
+                        FeedCard(item: item)
                     }
+                    
                 default: EmptyView()
             }
         }
