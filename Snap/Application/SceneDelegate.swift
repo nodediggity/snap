6aa71fc85d4e796d6c717a3f8d4a6dfa579bb281
Gordon Smith <iamgordonsmith@icloud.com>
2021-03-22 20:03:16 +0000

    feat: render tabs for home scene

--- Snap/Application/SceneDelegate.swift
+++ Snap/Application/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }()
     
     private lazy var navController: UINavigationController = {
-        let controller = UINavigationController(rootViewController: makeFeedScene())
+        let controller = UINavigationController(rootViewController: makeHomeScene())
         controller.navigationBar.isHidden = true
         return controller
     }()
@@ -65,10 +65,59 @@ private extension SceneDelegate {
     }
     
     // MARK:- Scenes
+    func makeHomeScene() -> UITabBarController {
+        let tabBarController = CustomTabBarController()
+        tabBarController.viewControllers = makeTabs(tabs: [.feed, .alerts, .messages, .profile])
+        
+        tabBarController.tabBar.tintColor = #colorLiteral(red: 0.2431372549, green: 0.6705882353, blue: 0.3607843137, alpha: 1)
+        tabBarController.tabBar.unselectedItemTintColor = .secondaryLabel
+
+        return tabBarController
+    }
+    
     func makeFeedScene() -> UIViewController {
         return FeedUIComposer.compose(
             loader: makeFeedLoader,
             imageLoader: makeImageLoader
         )
+    }
+    
+    func makeTabs(tabs: [Tabs]) -> [UINavigationController] {
+        let navControllers = tabs.indices
+            .map { index -> UINavigationController in
+                let tab = tabs[index]
+                let navController = UINavigationController()
+                navController.tabBarItem = .init(title: nil, image: tab.icon, selectedImage: nil)
+                
+                navController.tabBarItem.imageInsets = {
+                    switch index {
+                        case 0: return .init(top: 0, left: -32, bottom: 6, right: 0)
+                        case 1: return .init(top: 0, left: -48, bottom: 6, right: 0)
+                        case 2: return .init(top: 0, left: 0, bottom: 6, right: -56)
+                        case 3: return .init(top: 0, left: 0, bottom: 6, right: -36)
+                        default: return .zero
+                    }
+                }()
+                                                
+                return navController
+            }
+        
+        return zip(tabs, navControllers).map { (tab, navController) in
+            switch tab {
+                case .feed: navController.setViewControllers([makeFeedScene()], animated: false)
+                default: navController.setViewControllers([
+                    UIHostingController(rootView: Text("🍕"))
+                ], animated: false)
+            }
+            return navController
+        }
+    }
+}
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
     }
 }
